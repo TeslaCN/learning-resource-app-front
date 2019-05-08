9153b774abb4819c37c90dcf4f9e2d8f3e10ac0e
TESLA_CN <root496879118@me.com>
2019-05-08 10:41:20 +0800

    修改搜索结果展示效果

--- src/components/search/ResultView.vue
+++ src/components/search/ResultView.vue
@@ -1,17 +1,29 @@
 <template>
     <el-row>
         <a v-if="item.highlights.title && item.highlights.title[0]" class="result-title"
-           @click="onClick(item.resourceEntity)" :href="item.resourceEntity.source === 'spider' ? item.resourceEntity.url : '#/resource/' + item.resourceEntity.id" target="_blank">
+           @click="onClick(item.resourceEntity)"
+           :href="item.resourceEntity.source === 'spider' ? item.resourceEntity.url : '#/resource/' + item.resourceEntity.id"
+           target="_blank">
             <span v-html="item.highlights.title[0]"></span>
         </a>
         <a v-else class="result-title" @click="onClick(item.resourceEntity)" :href="item.resourceEntity.url"
            target="_blank">{{item.resourceEntity.title}}</a>
         <br>
+        <!--<span v-if="item.resourceEntity.tag.length > 0">文章标签 </span>-->
         <el-tag v-for="(tag, index) in item.resourceEntity.tag" :key="index" size="mini">{{tag}}</el-tag>
         <br>
-        <a class="result-url" @click="onClick(item.resourceEntity)" :href="item.resourceEntity.url" target="_blank">{{item.resourceEntity.url}}</a>
-        <div v-for="values in item.highlights">
-            <span v-for="value in values" v-html="value"></span>
+        <span>作者: </span><strong>{{item.resourceEntity.author}}</strong>&nbsp;&nbsp;
+        <span>发表日期: </span><strong>{{item.resourceEntity.date}}</strong>&nbsp;&nbsp;
+        <span>阅读量: </span><strong>{{item.resourceEntity.readCount}}</strong>&nbsp;&nbsp;
+        <br>
+        <!--<a class="result-url" @click="onClick(item.resourceEntity)" :href="item.resourceEntity.url" target="_blank">{{item.resourceEntity.url}}</a>-->
+        <!--<div v-for="values in item.highlights">-->
+        <!--<span v-for="value in values" v-html="value"></span>-->
+        <!--</div>-->
+        <span v-for="value in item.highlights.content" v-html="value"></span>
+        <div style="color: #a8a8a8;">
+            <span>索引时间: </span><span>{{new Date(item.resourceEntity.indexDate)}}</span>&nbsp;&nbsp;
+            <span>来源: </span><span>{{item.resourceEntity.source == 'spider' ? '爬虫':'站内发布'}}</span>
         </div>
     </el-row>
 </template>
@@ -25,7 +37,7 @@
             return {};
         },
         computed: {
-            isSignedIn(){
+            isSignedIn() {
                 return this.$store.getters.isSignedIn;
             }
         },
@@ -49,9 +61,11 @@
     em {
         color: red;
     }
+
     .result-title {
         /*text-decoration-line: blink;*/
     }
+
     .result-url {
         text-decoration: none;
     }
